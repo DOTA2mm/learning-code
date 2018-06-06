880ffb7e1078774e9b63f1aae05b45d63cb95019
Chuck <tengchenc@gmail.com>
2018-06-06 23:57:08 +0800

    implement interceptor

---
+++ interceptor/interceptor.js
@@ -0,0 +1,32 @@
+class Interceptor {
+  constructor() {
+    this.tasks = []
+  }
+  /**
+    use(async (context, next) => {
+      ...request...
+      await next()
+      ...response...
+    })
+   */
+  use(/* async */ functor) {
+    this.tasks.push(functor)
+  }
+  async run(context) {
+    const tasks = this.tasks
+
+    const proc = tasks.reduceRight((a, b) => async () => {
+      await b(context, a)
+    }, () => Promise.resolve())
+
+    try {
+      await proc()
+    } catch (ex) {
+      console.error(ex.message)
+    }
+
+    return context
+  }
+}
+
+module.exports = Interceptor;
